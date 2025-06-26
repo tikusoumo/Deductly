480387a9b5d41eb0a8fe61d2b6cdea9bd9576edd
sourav9098 <souravghosh2002gnit@gmail.com>
2025-06-26 11:18:21 +0530

    Update Headers in WebScrapper

--- chunking/webscrapper.py
+++ chunking/webscrapper.py
@@ -27,6 +27,14 @@ def scrape_blog_to_pdf(url, output_dir="data", filename=None):
     
     
     try:
+        headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Cache-Control': 'max-age=0'
+}
         response = requests.get(url)
         response.raise_for_status()
 
